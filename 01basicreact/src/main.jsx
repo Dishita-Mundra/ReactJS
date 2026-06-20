aa378b6f31ce4550aff9b81709c25210a272bd62
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-20 09:38:16 +0530

    basic-react

--- 01basicreact/src/main.jsx
+++ 01basicreact/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+
 function MyApp() {
   return (
     <div>
@@ -10,8 +11,20 @@ function MyApp() {
   )
 }
 
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+  <reactElement />
+
 )
