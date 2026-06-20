eb942e3f3bcf2c67296da38ac9b986be2cb3c325
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-20 14:37:07 +0530

    basic-react

---
+++ counter02/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  let counter = 5
+
+  const addValue = () => {
+    console.log("value");
+  }
+
+  return (
+    <>
+      <h1>Counter App</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>
+        Add Value</button> <br />
+
+      <button>Remove Value</button>
+    </>
+  )
+}
+
+export default App
