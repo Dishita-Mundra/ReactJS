669c08ba87fc5baa598d6d13b896d41654ce14ae
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-21 15:22:31 +0530

    tailwindcss

--- tailwindprops03/src/App.jsx
+++ tailwindprops03/src/App.jsx
@@ -10,36 +10,6 @@ function App() {
       <h1 className="max-w-2xl mx-auto bg-green-400 text-black p-4 rounded-xl">
         Tailwind test
       </h1>
-
-      <figure className="md:flex max-w-2xl mx-auto bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-        <img
-          className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
-          src={flowerImg}
-          alt=""
-          width="384"
-          height="512"
-        />
-
-        <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
-          <blockquote>
-            <p className="text-lg font-medium">
-              "Tailwind CSS is the only framework that I've seen scale
-              on large teams. It's easy to customize, adapts to any design,
-              and the build size is tiny."
-            </p>
-          </blockquote>
-
-          <figcaption className="font-medium">
-            <div className="text-sky-500 dark:text-sky-400">
-              Sarah Dayan
-            </div>
-
-            <div className="text-slate-700 dark:text-slate-500">
-              Staff Engineer, Algolia
-            </div>
-          </figcaption>
-        </div>
-      </figure>
     </>
   )
 }
