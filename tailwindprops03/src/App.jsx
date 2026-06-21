7d44f2b25d98cc5a1aa8aff9487d8ed34359af69
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-21 17:16:24 +0530

    tailwindcss

--- tailwindprops03/src/App.jsx
+++ tailwindprops03/src/App.jsx
@@ -1,15 +1,39 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from './assets/vite.svg'
-import flowerImg from './assets/flower.jpg'
 import './App.css'
 
 function App() {
   return (
     <>
-      <h1 className="max-w-2xl mx-auto bg-green-400 text-black p-4 rounded-xl">
+      <h1 className="max-w-2xl mx-auto bg-green-400 text-black p-4 rounded-xl mb-4">
         Tailwind test
       </h1>
+
+      <div className="relative h-50 w-50 rounded-md">
+        <img
+          src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=60"
+          alt="AirMax Pro"
+          className="z-0 h-50 w-50 rounded-md object-cover"
+        />
+
+        <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+
+        <div className="absolute bottom-4 left-4 text-left">
+          <h1 className="text-lg font-semibold text-white">
+            Delba
+          </h1>
+
+          <p className="mt-2 text-sm text-gray-300">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Excepturi, debitis?
+          </p>
+
+          <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+            View Profile →
+          </button>
+        </div>
+      </div>
     </>
   )
 }
