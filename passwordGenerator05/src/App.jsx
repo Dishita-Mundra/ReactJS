22f8502ea8d0a422994250f928a07c654fbe63af
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-23 23:41:06 +0530

    passwordGenerator

--- passwordGenerator05/src/App.jsx
+++ passwordGenerator05/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useCallback } from 'react'
-import './App.css'
 
 function App() {
   const [length, setLength] = useState(8);
@@ -14,9 +13,9 @@ function App() {
     if (numAll) str += "0123456789"
     if (charAll) str += "!@#$%^&*-_+=[]{}~`"
 
-    for (let i = 1; i <= array.length; i++) {
+    for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1)
-      pass = str.charAt(char)
+      pass += str.charAt(char)
 
     }
 
@@ -26,7 +25,44 @@ function App() {
 
   return (
     <>
-      <div className=''>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800'>
+
+        <h1 className='text-white text-center my-3'>
+          Password generator
+        </h1>
+
+        <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+
+          <input
+            type="text"
+            value={password}
+            className='outline-none w-full py-1 px-3 bg-white text-black'
+            placeholder='Password'
+            readOnly
+          />
+
+          <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>
+            copy
+          </button>
+
+        </div>
+
+        <div className='flex text-sm gap-x-2'>
+
+          <div className='flex items-center gap-x-1'>
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className='cursor-pointer'
+            />
+
+            <label>Length: {length}</label>
+
+          </div>
+
+        </div>
 
       </div>
     </>
