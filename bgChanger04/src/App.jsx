57861536ba56d1b4b5fc294a308f316ea1227e30
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-23 16:50:15 +0530

    bgChanger

--- bgChanger04/src/App.jsx
+++ bgChanger04/src/App.jsx
@@ -11,9 +11,20 @@ function App() {
       <div
         className="fixed flex flex-wrap 
         justify-center bottom-12 inset-x-0 px-2">
+
           <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-2 py-3 rounded-3xl'>
-            test
-            <button></button>
+
+            <button className='outline-none px-4 py-1 rounded-full text-white shadow-;lg'
+            style={{backgroundColor: "red"}}>
+            Red</button>
+
+            <button className='outline-none px-4 py-1 rounded-full text-white shadow-;lg'
+            style={{backgroundColor: "green"}}>
+            Green</button>
+
+            <button className='outline-none px-4 py-1 rounded-full text-white shadow-;lg'
+            style={{backgroundColor: "blue"}}>
+            Blue</button>
           </div>
       </div>
     </div>
