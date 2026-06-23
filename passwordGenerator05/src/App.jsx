4ff3da1bf96ac40fee45df11312338290227e64a
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-23 21:52:28 +0530

    passwordGenerator

--- passwordGenerator05/src/App.jsx
+++ passwordGenerator05/src/App.jsx
@@ -1,14 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useState, useCallback } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8);
+  const [numAll, setNumAll] = useState(false);
+  const [charAll, setCharAll] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    if (numAll) str += "0123456789"
+    if (charAll) str += "!@#$%^&*-_+=[]{}~`"
+
+    for (let i = 1;i <= array.length; i++) {
+      const element = arr[i];
+      
+    }
+
+  }, [length, numAll, charAll, setPassword])
 
   return (
-    <h1>Password Generator</h1>
+    <h1
+      className='text-4xl text-center text-white'
+    >
+      Password Generator</h1>
   )
 }
 
