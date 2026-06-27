9968d946da81d508ac092d25531855d1ed9ef95c
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-27 09:57:00 +0530

    reactRouter

--- reactRouter07/src/components/Github/Github.jsx
+++ reactRouter07/src/components/Github/Github.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react'
 
 function Github() {
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        fetch('https://api.github.com/users/Dishita-Mundra')
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                setData(data);
-            })
-    }, [])
+    // const [data, setData] = useState([]);
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/Dishita-Mundra')
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             console.log(data);
+    //             setData(data);
+    //         })
+    // }, [])
 
     return (
         <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'><a href="https://github.com/Dishita-Mundra">Github</a>
@@ -19,3 +19,8 @@ function Github() {
 }
 
 export default Github;
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/Dishita-Mundra');
+    return response.json();
+}
