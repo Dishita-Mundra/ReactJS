08999b326ad6edbded3bd1f79ab76ebd4073c853
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-28 18:29:23 +0530

    themeswitcher

--- themeswitcher09/src/App.jsx
+++ themeswitcher09/src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className='p-4 bg-pink-600 text-3xl'>Dishita</h1>
-    </>
+
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          {/* themeBtn */}
+
+        </div>
+
+        <div className="w-full max-w-sm mx-auto">
+          {/* Card */}
+
+        </div>
+      </div>
+    </div>
+
   )
 }
 
