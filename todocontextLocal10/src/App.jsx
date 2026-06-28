8627a944d85d94d5ce071620a52b11dd4fb602a6
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-06-28 20:53:09 +0530

    to-do

--- todocontextLocal10/src/App.jsx
+++ todocontextLocal10/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import { TodoProvider } from './contexts';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = () => { }
 
   return (
-    <>
+    <TodoProvider value={{ todos, addTodo, updatedTodo, deleteTodo, toggleComplete }}>
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
@@ -17,7 +20,7 @@ function App() {
           </div>
         </div>
       </div>
-    </>
+    </TodoProvider>
   )
 }
 
