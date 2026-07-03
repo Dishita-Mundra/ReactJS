d1f8823fdc9f46508ba164da16bd3508986d9979
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-07-03 13:36:52 +0530

    to-do

--- todocontextLocal10/src/App.jsx
+++ todocontextLocal10/src/App.jsx
@@ -56,7 +56,7 @@ function App() {
               <div key={todo.id}
                 className="w-full"
               >
-                <TodoItem todo={todo}/>
+                <TodoItem todo={todo} />
               </div>
             ))}
           </div>
