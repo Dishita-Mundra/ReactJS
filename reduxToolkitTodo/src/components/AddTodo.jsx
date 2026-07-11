d9d8ca179210e9c12b07c66042aafc9e70f3bdfd
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-07-11 18:44:39 +0530

    reactRedux

--- reduxToolkitTodo/src/components/AddTodo.jsx
+++ reduxToolkitTodo/src/components/AddTodo.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
+import { addTodo } from '../features/todo/todoSlice'
 
 function AddTodo() {
 
@@ -8,6 +9,8 @@ function AddTodo() {
 
     const addTodoHandler = (e) => {
         e.preventDefault()
+        dispatch(addTodo(input))
+        setInput('')
     }
 
     return (
