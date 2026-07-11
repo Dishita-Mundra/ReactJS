56c93125c81bc61bc9458c02dd869e7ab0989660
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-07-11 23:56:29 +0530

    reactRedux

--- reduxToolkitTodo/src/App.jsx
+++ reduxToolkitTodo/src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +9,8 @@ function App() {
   return (
     <>
       <h1>Learn about Redux Toolkit</h1>
+      <AddTodo />
+      <Todos />
     </>
   )
 }
