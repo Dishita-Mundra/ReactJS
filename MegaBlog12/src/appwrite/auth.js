f612a8229264ce93c72bf7fd8fbd329ff916281e
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-07-16 21:33:46 +0530

    MegaBlog

--- MegaBlog12/src/appwrite/auth.js
+++ MegaBlog12/src/appwrite/auth.js
@@ -1,17 +1,28 @@
 import conf from '../conf.js';
 import { Client, Account, ID } from "appwrite";
 
-export class AuthService {
-    client = new Client();
-    account;
+// Blueprint for creating an authentication service
+export class AuthService {  
+    
+    // Creates an Appwrite client instance
+    client = new Client();   
+
+    // Stores the Account service instance
+    account;   
 
     constructor() {
         this.client
-        .setEndpoint(conf.endpoint)
-        .setEndpoint(conf.projectId)
+
+            // Configure the Appwrite server URL
+            .setEndpoint(conf.appwriteUrl)   
+            .setProject(conf.appwriteProjectId);
+
+        // Create the Account service using the configured client
+        this.account = new Account(this.client);  
     }
 }
 
-const authService = new AuthService();
+// Singleton instance shared across the entire application
+const authService = new AuthService();  
 
-export default AuthService;
+export default authService;
