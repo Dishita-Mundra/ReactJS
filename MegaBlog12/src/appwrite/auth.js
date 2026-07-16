d09f97209eabc7733add8bed44a1a57760b8d108
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-07-16 23:51:37 +0530

    MegaBlog

--- MegaBlog12/src/appwrite/auth.js
+++ MegaBlog12/src/appwrite/auth.js
@@ -66,7 +66,7 @@ export class AuthService {
         try {
             return await this.account.deleteSessions();
         } catch (error) {
-            console.log("Appwrite service :: getCurrentUser :: error", error);
+            console.log("Appwrite service :: logout :: error", error);
         }
     }
 }
--- MegaBlog12/src/appwrite/config.js
+++ MegaBlog12/src/appwrite/config.js
@@ -5,12 +5,22 @@ export class Service {
     client = new Client();
     databases;
     bucket;
+
     constructor() {
         this.client
 
             // Configure the Appwrite server URL
             .setEndpoint(conf.appwriteUrl)
             .setProject(conf.appwriteProjectId);
+        this.databases = new Databases(this.client);
+    }
+
+    async createPost({ title, slug, content, featuredImage, status, userId }) {
+        try {
+            return await this.databases.createDocument();
+        } catch (error) {
+            console.log("Appwrite service :: createPost :: error", error);
+        }
     }
 }
 
