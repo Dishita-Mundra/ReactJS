2be082277a13783d6ead7f7bf0b5f7bc4c498c77
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-07-17 22:35:22 +0530

    MegaBlog

--- MegaBlog12/src/appwrite/config.js
+++ MegaBlog12/src/appwrite/config.js
@@ -81,20 +81,52 @@ export class Service {
         }
     }
 
-    async getPosts(queries = [Query.equal("status", "active")]) { 
+    async getPosts(queries = [Query.equal("status", "active")]) {
         try {
             return await this.databases.listDocuments(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
                 queries,
-                100,
-                0,
-
             )
         } catch (error) {
             console.log("Appwrite service :: getPosts :: error", error);
             return false;
         }
+    }
+
+    // file upload service
+
+    async uploadFile(file) {
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file,
+            )
+        } catch (error) {
+            console.log("Appwrite service :: uploadFile :: error", error);
+            return false;
+        }
+    }
+
+    async deleteFile(fileId) {
+        try {
+            return await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite service :: deleteFile :: error", error);
+            return false;
+        }
+    }
+
+    getFilePreview(fileId) {
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
     }
 }
 
