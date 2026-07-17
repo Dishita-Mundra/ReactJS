e1c263bf35e8eddbc6865af3abcfaa5bb64aef22
Dishita-Mundra <dishitamundra13574@gmail.com>
2026-07-17 15:50:36 +0530

    MegaBlog

--- MegaBlog12/src/appwrite/config.js
+++ MegaBlog12/src/appwrite/config.js
@@ -17,9 +17,54 @@ export class Service {
 
     async createPost({ title, slug, content, featuredImage, status, userId }) {
         try {
-            return await this.databases.createDocument();
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            );
         } catch (error) {
             console.log("Appwrite service :: createPost :: error", error);
+        }
+    }
+
+    async updatePost(slug, { title, content, featuredImage, status }) {
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+            );
+        } catch (error) {
+            console.log("Appwrite service :: updatePost :: error", error);
+        }
+    }
+
+    async deletePost(slug){
+        
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+            return true;
+
+        } catch (error) {
+            console.log("Appwrite service :: deletePost :: error", error);
+            return false;
         }
     }
 }
